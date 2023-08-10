3d5453a7bb7d3f5412dd2ae61b22fef20ec7828f
Fagundes1993 <mfbl93@gmail.com>
2023-08-09 22:53:55 -0300

    Adição das aulas de estrutura de repetição

---
+++ 16_estrutura_repeticao_for.py
@@ -0,0 +1,12 @@
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+#exemplo utilizando um iteravel
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+print() # adiciona uma quebra de linha
+
+#exemplo utilizando a função built-in range
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
